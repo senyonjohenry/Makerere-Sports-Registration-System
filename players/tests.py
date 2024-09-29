bd251ca93b78374b9b177213e47cfb8df126f838
SENYONJO HENRY <senyonjohenry1@gmail.com>
2024-09-29 12:13:43 +0300

    Downloaded and installed bootstrap to run locally in the project

--- players/tests.py
+++ players/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+
+def test_static(request):
+    result = ''
+    if finders.find('css/bootstrap.min.css'):
+        result += 'Bootstrap CSS found.<br>'
+    else:
+        result += 'Bootstrap CSS not found.<br>'
+
+    if finders.find('js/bootstrap.bundle.min.js'):
+        result += 'Bootstrap JS found.<br>'
+    else:
+        result += 'Bootstrap JS not found.<br>'
+    
+    return HttpResponse(result)
